72412a87eca1530337ba39006779e35eb9dd7186
Eduardo Medina <emedinaa@gmail.com>
2019-12-09 20:18:43 -0500

    Update screen 03

--- Project2/app/src/main/java/com/emedinaa/samples/Screen03Activity.kt
+++ Project2/app/src/main/java/com/emedinaa/samples/Screen03Activity.kt
@@ -2,11 +2,21 @@ package com.emedinaa.samples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_screen03.*
 
 class Screen03Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen03)
+
+
+        button.setOnClickListener {
+            back()
+        }
+    }
+
+    private fun back(){
+        finish()
     }
 }
